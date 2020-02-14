c09813b1567c5a34320f57d6c35e57589033843f
Vladimir Jotov <v.jotov@gmail.com>
2020-02-14 13:41:34 +0700

    fixing after 2nd merge request feedback

--- src/test/java/com/company/AnagramTest.java
+++ src/test/java/com/company/AnagramTest.java
@@ -1,21 +1,12 @@
 package com.company;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.function.Executable;
-
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class AnagramTest {
-    @Test
-    void methodExistTest () {
-        Class[] cArg = new Class[1];
-        cArg[0] = String.class;
 
-        assertAll((Executable) () -> Anagram.class.getMethod("process", cArg));
-
-    }
     @Test
     void nullValueTest() {
         assertThrows(IllegalArgumentException.class, () -> {Anagram.process(null);});
